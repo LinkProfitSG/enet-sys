0ad62f9dcd1513553da0ade43864fb2f38f57d0a
LinkProfitSG <azusagawakotoha@gmail.com>
2026-05-28 12:09:04 +0800

    Fix submodule bug

--- examples/host.rs
+++ examples/host.rs
@@ -4,32 +4,41 @@ use enet_sys::ENetAddress;
 use enet_sys::{enet_deinitialize, enet_initialize};
 use enet_sys::{enet_host_create, enet_host_destroy};
 
+use std::net::Ipv4Addr;
+
 fn main() {
     println!("Starting test of host creation...");
     if unsafe { enet_initialize() } < 0 {
         panic!("Error on enet initialization.");
     }
+
     println!("Enet initialized.");
 
     println!("creating ENet server host...");
 
     // default localhost on port 12345
     let address = ENetAddress {
-        host: 0,
-        port: 12345,
+        type_: 0,
+        host: enet_sys::_ENetAddress__bindgen_ty_1 { v4: Ipv4Addr::new(127, 0, 0, 1).octets() },
+        port: 8080,
     };
+
+    println!("Creating host for IP {:?}", Ipv4Addr::from(Ipv4Addr::LOCALHOST).octets());
 
     unsafe {
         let server = enet_host_create(
+            0,
             &address, // address to bind the server host to
             32,       // allow up to 32 clients and/or outgoing connections
             2,        // allow up to 2 channels to be used, 0 and 1
             0,        // assume any amount of incoming bandwidth
             0,
         ); // assume any amount of outgoing bandwidth
+
         if server.is_null() {
-            panic!("error: host create returned null")
+            panic!("Unable to create server host")
         }
+
         println!("...ENet server host created");
 
         println!("server: {:p}", server);
